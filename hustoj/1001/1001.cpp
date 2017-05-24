8382569211a4b44735fb4b860c2c4a292c1ba014
LiTingjun <739142578@qq.com>
2017-05-24 16:43:43 +0800

    initial commit

---
+++ hustoj/1001/1001.cpp
@@ -0,0 +1,36 @@
+/*
+1000 - A+B
+URL: http://acm.hust.edu.cn/problem/show/1000
+
+DESCRIPTION
+Calculate a + b
+
+INPUT
+The input will consist of a series of pairs of integers a and b,separated by a space, one pair of integers per line.
+
+OUTPUT
+For each pair of input integers a and b you should output the sum of a and b in one line,and with one line of output for each line in input.
+
+SAMPLE INPUT
+1 5
+
+SAMPLE OUTPUT
+6
+
+HINT
+
+SOURCE
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+    cout << a+b;
+    return 0;
+}
+
+
