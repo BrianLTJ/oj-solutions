0bbfc18595c8f14e5ac77fdb41cff0e2edc0f331
Tingjun Li <739142578@qq.com>
2018-02-22 16:08:18 +0800

    Add Algorithm Note and CodeUp

---
+++ codeup/100000568/25586-Problem-A.cpp
@@ -0,0 +1,12 @@
+/*
+http://codeup.hustoj.com/problem.php?cid=100000568&pid=0
+*/
+#include<stdio.h>
+
+int main(){
+int i=0,sum=0;
+while(i<=100)
+    sum+=(i++);
+printf("%d\n", sum);
+return 0;
+}
