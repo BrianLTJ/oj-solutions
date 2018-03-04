65fa8bb1085eeb9f9cbfafe56f747ec5f323ad98
Tingjun Li <739142578@qq.com>
2018-03-04 21:19:28 +0800

    Alg-note 9.2 Problem A

--- codeup/Algnote-9.2-100000611/Problem-A/main.cpp
+++ codeup/Algnote-9.2-100000611/Problem-A/main.cpp
@@ -16,23 +16,14 @@ struct node {
 };
 
 node * rebuildTree(const string & preorder, const string & inorder){
-//node * rebuildTree(char pre[], char in[]){
-    //node * p = (node *)malloc(sizeof(node));
     node *p = new node;
-    //string preorder = pre;
-    //string inorder = in;
     if(preorder.length()==0||inorder.length()==0)  return nullptr;
     else {
         p->data = preorder[0];
         string leftpre = preorder.substr(1,inorder.find(p->data));
-        //printf("leftpre:%s\n", leftpre.c_str());
         string leftin = inorder.substr(0,inorder.find(p->data));
-        //printf("leftin:%s\n",leftin.c_str());
         string rightpre = preorder.substr(inorder.find(p->data)+1);
-        //printf("rightpre:%s\n", rightpre.c_str());
         string rightin = inorder.substr(inorder.find(p->data)+1);
-        //printf("rightin:%s\n",rightin.c_str());
-
         p->left = rebuildTree(leftpre, leftin);
         p->right = rebuildTree(rightpre, rightin);
     }
