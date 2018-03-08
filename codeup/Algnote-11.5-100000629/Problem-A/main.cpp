1e9520dc65af9b4ff6ba9b5cbc323c328f85de00
Tingjun Li <739142578@qq.com>
2018-03-08 18:27:47 +0800

    Alg-note 11.5 Problem A Not finished

--- codeup/Algnote-11.5-100000629/Problem-A/main.cpp
+++ codeup/Algnote-11.5-100000629/Problem-A/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
+const int MAXLEN = 5000;
+char orgStr[MAXLEN], procStr[MAXLEN];
+int mapProcOrg[MAXLEN];
+int dp[MAXINT][MAXINT]={0};
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cin >> orgStr;
+    // Preprocess
+    int orgStrLen = strlen(orgStr);
+    int i,j;
+    for(i=0;i<orgStrLen;i++){
+        if(isalpha(orgStr[i])){
+            procStr[j]=toupper(orgStr[i]);
+            mapProcOrg[j]=i;
+            j++;
+        }
+        if(isdigit(orgStr)){
+            procStr[j]=orgStr[i];
+            mapProcOrg[j]=i;
+            j++;
+        }
+    }
+    int procStrLen = strlen(procStr);
+    for(int i=0;i<procStrLen;i++){
+        for(int j=0;j<procStrLen;j++){
+
+        }
+    }
+
     return 0;
 }
