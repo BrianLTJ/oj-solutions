7274981c41015fd0e46634082e833f8de74a63b0
Tingjun Li <739142578@qq.com>
2018-03-14 22:02:55 +0800

    [UnTested] SJTU zigzag

--- realquestions/sjtu-zigzag/main.cpp
+++ realquestions/sjtu-zigzag/main.cpp
@@ -1,17 +1,53 @@
 #include <iostream>
 
-const int MAXENUM = 50;
 using namespace std;
 
-int main()
-{
-    int n;
-    int num[MAXENUM] = {0};
-    int numDiff[MAXENUM] = {0};
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> num[i];
-        if(i>0) numDiff[i-1] = num[i] - num[i-1];
-    }
-    return 0;
+const int MAXELEM = 50;
+
+struct zInfo{
+	int len;
+	int diff;
+	int prevElem;
+	zInfo() {
+		len = 1;
+		diff = 0;
+		prevElem = 0;
+	}
+};
+
+int main() {
+	int n;
+	int nums[MAXELEM]; // Store orig number
+	int maxZLen[MAXELEM] = {0};
+	zInfo z[MAXELEM];
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> nums[i];
+	}
+
+	if(n > 1) {
+		z[1].len = 2;
+		z[1].prevElem = 0;
+		z[1].diff = nums[1] - nums[0];
+	}
+
+	for(int i = 1; i < n; i++) {
+		for(int j = i - 1; j >= 0; j--) {
+			int expectedSign = - (z[j].diff);
+			if(expectedSign * (nums[i] - nums[j]) > 0 && z[j].len + 1 > z[i].len) {
+
+				z[i].len = z[j].len + 1;
+				z[i].prevElem = j;
+				z[i].diff = nums[i] - nums[j];
+
+				// cout << "Update z info for num "<<i<<", stick after " << j << ", current zlen " << z[i].len << endl;
+			}
+		}
+	}
+
+
+	cout << z[n-1].len;
+
+
+	return 0;
 }
