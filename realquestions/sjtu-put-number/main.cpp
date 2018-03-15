3c125d2aea83d712925b829852ef9de76009774e
Tingjun Li <739142578@qq.com>
2018-03-15 21:39:11 +0800

    [Unfinished] SJTU put number

---
+++ realquestions/sjtu-put-number/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const int MAXN = 10;
+
+int mat[MAXN][MAXN] = {0};
+int linesum[MAXN] = {0}, colSum[MAXN] = {0},
+
+int main(){
+	int n;
+	cin >> n;
+	int perUnit = (1 + n * n) * n / 2;
+
+	return 0;
+}
